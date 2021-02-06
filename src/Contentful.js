b01cbcd2770b3355cc32c8a1db6c5ffd84718ece
Sujay <alaspuresujay@gmail.com>
2021-02-06 14:22:30 +0530

    added backend by contentful secret removed

--- src/Contentful.js
+++ src/Contentful.js
@@ -2,6 +2,6 @@ import {createClient} from 'contentful'
 
 
 export default createClient({
-    space: '5ykvzylc9tqd',
-    accessToken: 'KswMVWwJ-e4US5WS8-Qf7cMeJgN7uH_UAPUzCnqImpw'
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
